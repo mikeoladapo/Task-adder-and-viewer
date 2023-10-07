2437a3ca6a44830a13ced0b138c62cd6180f6ff4
mikeoladapo <134233611+mikeoladapo@users.noreply.github.com>
2023-10-07 04:38:50 -0700

    Create toadd_andview.py

---
+++ toadd_andview.py
@@ -0,0 +1,44 @@
+class Task:
+    def __init__(self,title,date,time):
+        self.title=title
+        self.date=date
+        self.time=time
+        self.status=False
+    def add_to_list(self):
+        self.lis.append(self)
+        
+    def viewtasks(self):
+            print(f'Title: {task.title}, Date: {task.date}, Time: {task.time}')
+
+
+    def completed(self):
+        self.status=True
+ 
+tasklist=[]
+       
+handle1 = input('Enter title: ')
+handle2=input('Enter date: ')
+handle3=input('Enter time: ')
+
+
+tas=Task(handle1,handle2,handle3)
+tasklist.append(tas)
+for task in tasklist:
+    task.viewtasks()
+    
+
+
+
+
+
+
+
+
+    
+        
+              
+        
+        
+        
+        
+        
